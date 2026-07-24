59c400816efc612c82005564428e192d9c001f74
Lennart Köhler HKI <lennart.koehler@leibniz-hki.de>
2026-07-24 11:27:11 +0200

    fix: add f suffix to float literal initializers to avoid double truncation (C4305)

--- include/dolphin/psf/configs/GibsonLanniPSFConfig.h
+++ include/dolphin/psf/configs/GibsonLanniPSFConfig.h
@@ -23,45 +23,45 @@ public:
 	GibsonLanniPSFConfig(const GibsonLanniPSFConfig& other);
 
 
-    float OVER_SAMPLING = 4.0;
-    float lambda_nm = 520.0;
+    float OVER_SAMPLING = 4.0f;
+    float lambda_nm = 520.0f;
     int accuracy = 32;
     
     /** Working distance of the objective (design value). */
-    float ti0_nm = 150000.0;
+    float ti0_nm = 150000.0f;
 
     /** Working distance of the objective (experimental value). */
-    float ti_nm = 150000.0;
+    float ti_nm = 150000.0f;
 
     /** Immersion medium refractive index (design value). */
-    float ni0 = 1.515;
+    float ni0 = 1.515f;
 
     /** Immersion medium refractive index (experimental value). */
-    float ni = 1.515;
+    float ni = 1.515f;
 
     /** Coverslip thickness (design value). */
-    float tg0_nm = 170.0;
+    float tg0_nm = 170.0f;
 
     /** Coverslip thickness (experimental value). */
-    float tg_nm = 170.0;
+    float tg_nm = 170.0f;
 
     /** Coverslip refractive index (design value). */
-    float ng0 = 1.5;
+    float ng0 = 1.5f;
 
     /** Coverslip refractive index (experimental value). */
-    float ng = 1.5;
+    float ng = 1.5f;
 
     /** Sample refractive index. */
-    float ns = 1.33;
+    float ns = 1.33f;
 
     /** Axial position of the particle. */
-    float particleAxialPosition_nm = 1000.0;
+    float particleAxialPosition_nm = 1000.0f;
 
     /** Pixel size in axial direction. */
-    float pixelSizeAxial_nm = 100.0;
+    float pixelSizeAxial_nm = 100.0f;
 
     /** Pixel size in lateral direction. */
-    float pixelSizeLateral_nm = 100.0;
+    float pixelSizeLateral_nm = 100.0f;
 
     
 private:
