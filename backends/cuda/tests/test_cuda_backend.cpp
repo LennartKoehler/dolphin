4b3cb30da8e2b82ec7d78291ada114f4657f61e5
Lennart Köhler HKI <lennart.koehler@leibniz-hki.de>
2026-07-28 10:30:37 +0200

    fix: initializer list syntax

--- backends/cuda/tests/test_cuda_backend.cpp
+++ backends/cuda/tests/test_cuda_backend.cpp
@@ -39,7 +39,7 @@ protected:
         return manager;
     }
 
-    BackendConfig config{nThreads: 1, backendName: "cuda_test"};
+    BackendConfig config{1, "cuda_test"};
     IBackend* backend = nullptr;
 
     void SetUp() override {
